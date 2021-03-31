025d936558be01da5a076576ad3576547e640003
alice <3200211683@qq.com>
2021-03-31 21:18:37 +0800

    -移除多余的文件

--- sob_blog/Pods/Target Support Files/Pods-sob_blog-sob_blogUITests/Pods-sob_blog-sob_blogUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_sob_blog_sob_blogUITests : NSObject
-@end
-@implementation PodsDummy_Pods_sob_blog_sob_blogUITests
-@end
--- sob_blog/Pods/Target Support Files/Pods-sob_blog/Pods-sob_blog-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_sob_blog : NSObject
-@end
-@implementation PodsDummy_Pods_sob_blog
-@end
